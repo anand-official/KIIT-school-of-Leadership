96825784db5fb7b98294a934ce58b7266c283a0c
Ujjawal Anand <anandujjawalofficial11@gmail.com>
2026-02-18 08:20:50 +0000

    Update website: brochure links, partner logos, add news/student sections

---
+++ git-push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /workspaces/KIIT-school-of-Leadership
+git add -A
+git commit -m "Update website pages: brochure links, partner logos, and add news/student-development sections"
+git push origin main
